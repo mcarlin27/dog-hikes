b01c8005b3da35cc08bd1dd009509a5322245bb4
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-07-14 10:45:35 -0700

    initial app module/component ts/html set up; add firebase to project.

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyDH403OKx7YKFi1_ZYur4XNXRvu2gMaTpI",
+  authDomain: "k9-hiking.firebaseapp.com",
+  databaseURL: "https://k9-hiking.firebaseio.com",
+  projectId: "k9-hiking",
+  storageBucket: "k9-hiking.appspot.com",
+  messagingSenderId: "951707438254"
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AppComponent } from './app.component';
+// import { HomepageComponent } from './homepage/homepage.component';
+import { routing } from './app.routing';
+// import { MembersComponent } from './members/members.component';
+// import { MemberDetailComponent } from './member-detail/member-detail.component';
+// import { AdminComponent } from './admin/admin.component';
+// import { EditMemberComponent } from './edit-member/edit-member.component';
+// import { AdminPipe } from './admin.pipe';
+// import { NewMemberComponent } from './new-member/new-member.component';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // HomepageComponent,
+    // MembersComponent,
+    // MemberDetailComponent,
+    // AdminComponent,
+    // EditMemberComponent,
+    // AdminPipe,
+    // NewMemberComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
